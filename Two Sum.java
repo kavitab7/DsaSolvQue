ef86dc54ba478175afb7e1a91f634b3b231a7ffb
Kavita Bhosale <112501593+kavitab7@users.noreply.github.com>
2023-10-21 20:53:33 +0530

    Create Two Sum.java

---
+++ Two Sum.java
@@ -0,0 +1,17 @@
+https://www.youtube.com/watch?v=MxgBfF5ti8o&list=PLxgZQoSe9cg00xyG5gzb5BMkOClkch7Gr&index=65
+https://leetcode.com/problems/two-sum/description/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans ={-1};
+        HashMap<Integer , Integer> mp= new HashMap<>();
+    for(int i=0;i<nums.length;i++){
+        int partner = target - nums[i];
+        if(mp.containsKey(partner)){
+            ans = new int[]{i , mp.get(partner)};
+            return ans;
+        }
+        mp.put(nums[i] , i);
+    }
+    return ans;
+    }
+}
