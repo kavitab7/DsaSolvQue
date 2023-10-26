f05a299231c8ad2ce7e60abc64220bc2c7d9d2cf
Kavita Bhosale <112501593+kavitab7@users.noreply.github.com>
2023-10-26 20:11:36 +0530

    Update Maximum Subarray.java

--- Maximum Subarray.java
+++ Maximum Subarray.java
@@ -2,6 +2,23 @@ https://leetcode.com/problems/maximum-subarray/description/
 
 class Solution {
     public int maxSubArray(int[] nums) {
+        int min = Integer.MIN_VALUE; 
+        int ans = 0;
+        for(int i=0;i<nums.length ;i++){
+           ans = ans+ nums[i];
+           if(ans>min){
+               min= ans;
+           }
+          if(ans<0){
+            ans=0;
+        }
+        }
+        return min;
+    }
+}
+
+class Solution {
+    public int maxSubArray(int[] nums) {
         int maxSum = nums[0];
         int sum = nums[0];
         for(int i =1;i<nums.length;i++){
